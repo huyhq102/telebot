756f27f9ee7b0fa53d5073988810f96a53432fe4
unknown <huyhq102@gmail.com>
2024-07-16 01:13:34 +0700

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     // host: 'http://localhost:4000/',
     host: 'https://my-first-project-production-9b2c.up.railway.app/',
     // host: 'http://192.168.1.13:4000/',
