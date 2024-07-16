b81e20b592b793dc573d6a5dc2fc730f8663523c
Dev <dev@Devs-MacBook-Pro.local>
2024-07-16 13:54:47 +0700

    feats: fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  totalActivePoint = 0;
+  totalPoint = 0
 
   ngOnInit(): void {
+    this.loadActivePoint()
+    this.loadTotalPoint()
   }
 
+  loadActivePoint() {
+    const tg = (window as any).Telegram.WebApp;
+    tg.ready();
+    tg.expand();
+
+    const userInfo = tg.initDataUnsafe;
+
+    this.httpClient.get(`https://my-first-project-production-9b2c.up.railway.app/users/${userInfo?.user?.id}`).pipe().subscribe(data => {
+      this.totalActivePoint = Number(data);
+    })
+  }
+
+  loadTotalPoint() {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "https://my-first-project-production-9b2c.up.railway.app/users",
+    };
+
+    axios.request(config).then(response => {
+      this.totalPoint = response.data.reduce((total: any, obj: { points: any; }) => obj.points + total, 0);
+    })
+  }
+
+  gotoPage(page = '') {
+    window.open(page)
+  }
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  // private router:  Router;
 
-  constructor() { }
+  constructor(private router:Router) { 
+
+  }
 
   ngOnInit(): void {
   }
-
+  goToScore() {
+    this.router.navigate(['/scores'])
+  }
 }
