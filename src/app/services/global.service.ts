d46022f91d865d33af5d26a27502462c734d1f3e
Dev <dev@Devs-MacBook-Pro.local>
2024-07-22 17:23:59 +0700

    feats: update UI

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -8,26 +8,26 @@ userInfo: any;
 constructor(private http: HttpClient) {}
 
     loadUserInfo() {
-        return {
-            user: {
-                id: '1893048886',
-                first_name: 'Nguyen',
-                last_name: 'Son'    
-            },
-             start_param: '1804277515'
-        }
-
-        // if (this.userInfo) {
-        //     return this.userInfo;
-        // } else {
-        //     const tg = (window as any).Telegram.WebApp;
-
-        //     tg.ready();
-        //     tg.expand();
-            
-        //     this.userInfo = tg.initDataUnsafe;
-
-        //     return this.userInfo
+        // return {
+        //     user: {
+        //         id: '1893048886',
+        //         first_name: 'Nguyen',
+        //         last_name: 'Son'    
+        //     },
+        //      start_param: '1804277515'
         // }
+
+        if (this.userInfo) {
+            return this.userInfo;
+        } else {
+            const tg = (window as any).Telegram.WebApp;
+
+            tg.ready();
+            tg.expand();
+            
+            this.userInfo = tg.initDataUnsafe;
+
+            return this.userInfo
+        }
     }
 }
