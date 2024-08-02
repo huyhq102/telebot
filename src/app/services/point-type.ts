7adeb5a60410c9dea4a81a71e7fd2301e3754a48
Dev <dev@Devs-MacBook-Pro.local>
2024-08-02 11:13:53 +0700

    feats: intergrate apis on earn page

---
+++ src/app/services/point-type.ts
@@ -0,0 +1,30 @@
+export enum PointEntityType {
+    FirstJoinApp = 0,
+    JoinTelegramGroup = 1,
+    JoinTelegramChannel = 2,
+    InviteFriend = 3,
+    FollowX = 4,
+    LikeStudihubX = 5,
+    SharePostOnX = 6,
+    Chat10Messages = 7,
+    Add3Friends = 8,
+    Tag3MembersOnPinnedMessage = 9,
+    GotIconOnTelegramName = 10,
+    DailyLuckyWheel = 11,
+    PuzzleGame = 12,
+}
+
+export const PointAmount = {
+    0 : 1000,
+    1 : 3000,
+    2 : 3000,
+    4 : 3000,
+    5 : 2000,
+    6 : 2000,
+    7 : 3000,
+    8 : 3000,
+    9 : 3000,
+    10: 1000,
+    11: 1000,
+    12: 1000,
+}
